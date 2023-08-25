005a22adad230c9930145648cafb51ba314f338f
Brandon Guerrero <brandon_g909@yahoo.com>
2023-08-24 22:06:59 -0700

    edited portfolio

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 
 document.getElementById("hire-me-btn").addEventListener("click", function() {
     // Replace this code with your own code to handle the button click
-    alert("Thank you for your interest in hiring me!");
+    alert("Thank you for your interest in hiring me! Plese reach out to me through any of my listed forms of contact below!");
   });
   
 document.getElementById("submit-btn").addEventListener("click", function(event) {
